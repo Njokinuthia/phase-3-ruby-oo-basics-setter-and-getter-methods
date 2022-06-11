85c16e90684bf3915e716b9ad2c861f5b1aee943
Njoki <kinuthiash@gmail.com>
2022-06-11 21:38:56 +0300

    commit tree 5d53b579b628b5894cac6b8544c54df0d7480072

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
-class Dog
+class Dog  
 
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed= dog_breed
+  end
+
+  def breed
+    @breed
+  end
 end
+
+fido = Dog.new
+fido.name="scooby"
+fido.breed="dobberman"
+puts fido.name
+puts fido.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+  def job
+    @job
+  end
 end
+
+njoki = Person.new
+njoki.name="Njoki Kinuthia"
+njoki.job="software engineer"
+
+puts njoki.name
+puts njoki.job
